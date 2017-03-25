79da866335787f41295ab6d732acb6690a82c196
psyrp1 <psyrp1@exmail.nottingham.ac.uk>
2017-03-25 18:27:43 +0000

    spaceship now faces cursor

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var mainState = {
 
     preload: function() { //for loading assets etc
         //load the main rocket image and save as 'player'
-        game.load.image('player', 'assets/PNG/Sprites/Ships/spaceShips_007.png'); 
+        game.load.image('player', 'assets/PNG/Sprites/Ships/spaceship.png'); 
     },
 
     create: function() {
@@ -13,6 +13,9 @@ var mainState = {
         game.world.enableBody = true;
 
         this.player = game.add.sprite(32, 32, 'player');this.player.scale.setTo(0.5,0.5); //add and rescale
+        this.player.anchor.setTo(0.5,0.5);
+        // game.physics.enable(this.player, Phaser.Physics.ARCADE);
+        this.player.body.allowRotation = false;
         this.cursor = game.input.keyboard.createCursorKeys(); //cursor object to detect key presses
 
         this.wasd = {
@@ -26,6 +29,7 @@ var mainState = {
     },
     update: function() {
         //setting initial speed and moving speed
+        this.player.rotation = game.physics.arcade.angleToPointer(this.player);
         var speed = 322;//moving speed
         this.player.body.velocity.y = 0;
         this.player.body.velocity.x = 0;
