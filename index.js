2e209fa5fa796a3948c4903e245437463e4d4f92
psyrp1 <psyrp1@exmail.nottingham.ac.uk>
2017-03-25 17:01:02 +0000

    loaded image onto screen

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-var game = new Phaser.Game(500,500);
+var game = new Phaser.Game(600,800);
+
+var mainState = {
+
+    preload: function() { //for loading assets etc
+        //load the main rocket image and save as 'player'
+        game.load.image('player', 'assets/PNG/Sprites/Ships/spaceShips_007.png'); 
+    },
+
+    create: function() {
+        game.stage.backgroundColor = '#040114'; //change background colour
+        game.physics.startSystem(Phaser.Physics.ARCADE); //setting physics type
+        game.world.enableBody = true;
+
+        this.player = game.add.sprite(32, 32, 'player');this.player.scale.setTo(0.5,0.5); //add and rescale
+        this.cursor = game.input.keyboard.createCursorKeys(); //cursor object to detect key presses
+
+        var speed = 250;
+        this.player.body.velocity.y = 0;
+        this.player.body.velocity.x = 0;
+
+    },
+    update: function() {
+    }
+};
+
+game.state.add('main', mainState);
+game.state.start('main');
