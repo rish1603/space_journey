b717badf92d8af44e7e6c7f2af6a4ef7a6deba7d
Jonathan Balls <j.balls@btopenworld.com>
2017-03-26 13:38:23 +0100

    Fix ogship link

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ var mainState = {
     preload: function() { //for loading assets etc
         //load the main rocket image and save as 'player'
         game.load.image('player', 'assets/PNG/' + theme + '/Ships/spaceship.png'); 
-        game.load.image('OGplayer', 'assets/PNG/' + theme + '/Ships/ogship.png'); 
+        game.load.image('OGplayer', 'assets/PNG/Shit/Ships/ogship.png'); 
         game.load.image('4way', 'assets/PNG/' + theme + '/Ships/4way.png'); 
         game.load.image('boss', 'assets/PNG/Shit/Ships/boss.png'); 
         game.load.image('meteor', 'assets/PNG/Sprites/Meteors/spaceMeteors_001.png'); 
